5c63ac76542cbe5f9f6462aa81004b20d469a6c1
milanmenezes <milanmenezes@gmail.com>
2016-10-22 08:34:37 +0530

    3rd question changed data type to long long int

--- Inti_Sets.cpp
+++ Inti_Sets.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-
+#include <new>
 using namespace std;
-
-
 
 int main()
 {
-   int q,n,a,b,f=1,c=0;
-   int *x;
+   int q,f,c;
+   long long int n,a,b,sum;
+   long long int *x;
    cin>>q;
    for(int l=0;l<q;l++){
     cin>>n>>a>>b;
-    x= new int[n];
-    for(int i=1;i<=n;i++){
+    sum=0;
+    c=0;
+    x= new long long int[n];
+    for(long long int i=1;i<=n;i++){
             f=1;
-        for(int j=2;j<=i;j++)
+        for(long long int j=2;j<=i;j++)
         if(i%j==0 && n%j==0){
             f=0;
             break;
@@ -23,11 +24,16 @@ int main()
         {
             x[c]=i;
             c++;
-            cout<<i<<" ";
         }
     }
-    for()
+    for(int j=0;x[j]<=b && j<c;j++)
+    {
+        if(x[j]>=a)
+            sum+=x[j];
+    }
 
+    sum=sum%1000000007;
+    cout<<sum<<endl;
     delete []x;
    }
     return 0;
